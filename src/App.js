566a4baf3eb345d544ccd9dca630f704d2afa040
Yashraj Pravin Desale <119423317+BKYashraj@users.noreply.github.com>
2024-01-07 03:27:16 +0530

    remove logos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
