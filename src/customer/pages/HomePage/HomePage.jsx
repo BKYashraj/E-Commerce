a25bd25f21242ac124b4dc08af9dc101622649c9
Yashraj Pravin Desale <119423317+BKYashraj@users.noreply.github.com>
2024-01-07 03:16:19 +0530

    2nd commit

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import MainCarousel from "../../components/Homecarousel/MainCarousel";
+import HomeSectionCarousel from "../../components/HomeSectionCarousel/HomeSectionCarousel";
+import { mens_kurta } from "../../../Data/mens_kurta";
+
+function HomePage() {
+  return (
+    <div>
+      <MainCarousel />
+
+      <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
+        <HomeSectionCarousel data={mens_kurta}/>
+        <HomeSectionCarousel data={mens_kurta}/>
+        <HomeSectionCarousel data={mens_kurta}/>
+        <HomeSectionCarousel data={mens_kurta}/>
+        <HomeSectionCarousel data={mens_kurta}/>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
